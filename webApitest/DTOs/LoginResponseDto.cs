b97f89706469a1c6f00ea06363828fee561b3f1f
Vinod9879 <vinodreddy9879@gmail.com>
2025-09-24 00:11:55 +0530

    Delete webApitest directory

--- webApitest/DTOs/LoginResponseDto.cs
+++
@@ -1,8 +0,0 @@
-namespace webApitest.DTOs
-{
-    public class LoginResponseDto
-    {
-        public string Token { get; set; } = string.Empty;
-        public UserResponseDto User { get; set; } = new UserResponseDto();
-    }
-}
